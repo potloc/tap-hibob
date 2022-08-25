6a1e17398e9b0aa252abbb4476461701bcacd707
vinh <vinh94@hotmail.fr>
2022-08-25 16:27:06 +0200

    DE-428 Add id to schema

--- tap_hibob/schemas/EmployeePayroll.py
+++ tap_hibob/schemas/EmployeePayroll.py
@@ -2,6 +2,7 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 
 schema = th.PropertiesList(
     th.Property("fullName", th.StringType),
+    th.Property("id", th.StringType),
     th.Property("creationDate", th.StringType),
     th.Property("displayName", th.StringType),
     th.Property("canBeDeleted", th.BooleanType),
