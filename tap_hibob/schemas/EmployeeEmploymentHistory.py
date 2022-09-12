456092d99c6b30302d2e608e4d2a52594834c1ea
Stephane Burwash <stephane.burwash@potloc.com>
2022-09-12 14:38:19 -0400

    Created employment history schema

---
+++ tap_hibob/schemas/EmployeeEmploymentHistory.py
@@ -0,0 +1,27 @@
+from singer_sdk import typing as th  # JSON Schema typing helpers
+
+schema = th.PropertiesList(
+    th.Property("id", th.IntegerType),
+    th.Property("effectiveDate", th.StringType),
+    th.Property("endEffectiveDate", th.StringType),
+    th.Property("isCurrent", th.BooleanType),
+    th.Property(
+        "change",
+        th.ObjectType(
+            th.Property("reason", th.StringType),
+            th.Property("changedBy", th.StringType),
+            th.Property("changedById", th.StringType),
+        ),
+    ),
+    th.Property("creationDate", th.StringType),
+    th.Property("modificationDate", th.StringType),
+    th.Property("contract", th.StringType),
+    th.Property("type", th.StringType),
+    th.Property("calendarName", th.StringType),
+    th.Property("calendarId", th.IntegerType),
+    th.Property("salaryPayType", th.StringType),
+    th.Property("fte", th.IntegerType),
+    th.Property("weeklyHours", th.IntegerType),
+    th.Property("activeEffectiveDate", th.StringType),
+    th.Property("employee_id", th.StringType),
+).to_dict()
