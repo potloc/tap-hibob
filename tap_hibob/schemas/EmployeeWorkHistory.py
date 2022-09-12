8755d1683f88665184e6f4d5e7a105eed4f09018
Stephane Burwash <stephane.burwash@potloc.com>
2022-09-12 14:38:34 -0400

    Created employee work history

--- tap_hibob/schemas/EmployeeWorkHistory.py
+++ tap_hibob/schemas/EmployeeWorkHistory.py
@@ -2,8 +2,11 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 
 schema = th.PropertiesList(
     th.Property("id", th.IntegerType),
+    th.Property("employee_id", th.StringType),
     th.Property("effectiveDate", th.StringType),
     th.Property("endEffectiveDate", th.StringType),
+    th.Property("activeEffectiveDate", th.StringType),
+    th.Property("modificationDate", th.StringType),
     th.Property("isCurrent", th.BooleanType),
     th.Property(
         "change",
@@ -13,15 +16,8 @@ schema = th.PropertiesList(
             th.Property("changedById", th.StringType),
         ),
     ),
-    th.Property("creationDate", th.StringType),
-    th.Property("modificationDate", th.StringType),
-    th.Property("contract", th.StringType),
-    th.Property("type", th.StringType),
-    th.Property("calendarName", th.StringType),
-    th.Property("calendarId", th.IntegerType),
-    th.Property("salaryPayType", th.StringType),
-    th.Property("fte", th.IntegerType),
-    th.Property("weeklyHours", th.IntegerType),
-    th.Property("activeEffectiveDate", th.StringType),
-    th.Property("employee_id", th.StringType),
+    th.Property("site", th.StringType),
+    th.Property("title", th.StringType),
+    th.Property("department", th.StringType),
+    th.Property("workChangeType", th.StringType)
 ).to_dict()
