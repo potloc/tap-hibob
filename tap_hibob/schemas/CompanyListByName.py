1d5de99297e9a8355275ed2ebcdbd034c28e9ec9
Stéphane Burwash <sburwash@live.ca>
2023-06-20 15:44:52 -0400

    Added the list id to child strem

--- tap_hibob/schemas/CompanyListByName.py
+++ tap_hibob/schemas/CompanyListByName.py
@@ -1,6 +1,7 @@
 from singer_sdk import typing as th  # JSON Schema typing helpers
 
 schema = th.PropertiesList(
+    th.Property("list_id", th.StringType),
     th.Property("id", th.StringType),
     th.Property("value", th.StringType),
     th.Property("name", th.StringType),
